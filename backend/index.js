e4c19db0c23518efb067a947acbfd524f37e10bd
Ashish Singh <ashish.21bce7889@vitapstudent.ac.in>
2025-05-04 18:46:01 +0530

    registerAndLoginWorking

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js';
 
 dotenv.config({ path: './.env' });
 const app = express();
